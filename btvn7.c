94674284e071e3568a5f6fc6cfee280f9dd96ec3
WaterproofPaint22 <maiquanminh220407@gmail.com>
2025-10-19 22:30:23 +0700

    Add files via upload

--- btvn7.c
+++ btvn7.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
-#include <math.h>
+
 int main(){
-	int number = 12345, number1, number2, Sum ;
-	number1 = number % 2;
-	number2 = number / 2;
-	Sum = number1 + number2;
+	int number = 12345, Sum = 0 ;
+	while (number > 0) {
+	Sum += number % 10;
+	number = number / 10;
+	}
 	printf ("Tong cua hai so la: %d", Sum);
 	return 0;
 	 
 	
 }
+
